caad6c2e9f6f3ee60391c9845cbe2df0bbc15006
acsant <acsant@uwaterloo.ca>
2017-06-07 13:55:00 -0400

    changed add content

--- buildnetwork.sh
+++ buildnetwork.sh
@@ -1,4 +1,4 @@
 ./addpeer.sh
-./addpeer.sh 192.168.0.103 10000
-./addpeer.sh 192.168.0.103 10001
-./addpeer.sh 192.168.0.103 10000
+./addpeer.sh 10.81.112.86 10000
+./addpeer.sh 10.81.112.86 10001
+./addpeer.sh 10.81.112.86 10000
